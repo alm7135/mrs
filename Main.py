236699faeeaba3c0ff705fb6efcdf8a58a98f2ae
alm7164 <edgarasamanagement@gmail.com>
2020-03-27 13:44:57 +0200

    added sound output for encrypted text

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
 import Decrypt as morse
+import winsound
+import time
 
 
 def main():
     message = input('enter text: ')
     result = morse.encrypt(message.upper())
     print(result)
+    for charachter in result:
+        if charachter == '.':
+            winsound.Beep(400, 500)
+            time.sleep(0.5)
+        elif charachter == '-':
+            winsound.Beep(1000, 500)
+            time.sleep(0.5)
+        elif charachter == ' ':
+            time.sleep(1)
 
 
 if __name__ == "__main__":
