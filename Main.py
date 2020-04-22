4392fe50313aed4082bef9c375fac711410ded30
Augustas <gugustas142@gmail.com>
2020-04-22 16:25:40 +0300

    Prideta nauja eilute

--- Main.py
+++ Main.py
@@ -16,7 +16,7 @@ def main():
             time.sleep(0.5)
         elif charachter == ' ':
             time.sleep(1)
-    Lba diena neigeairiira jega
+    Sveiki cia naujas bandymas
 
 if __name__ == "__main__":
     main()
