2452a690b2d55d55c6cd3b27a63389824f1d9662
alm7164 <edgarasamanagement@gmail.com>
2020-04-28 20:52:16 +0300

    cleaned up main syntax

--- Main.py
+++ Main.py
@@ -1,22 +1,14 @@
 import Decrypt as morse
-import winsound
-import time
-
+import ToneSound as ts
 
 def main():
     message = input('enter text: ')
     result = morse.encrypt(message.upper())
     print(result)
     # Play sound in windows
-    for charachter in result:
-        if charachter == '.':
-            winsound.Beep(400, 500)
-            time.sleep(0.5)
-        elif charachter == '-':
-            winsound.Beep(1000, 500)
-            time.sleep(0.5)
-        elif charachter == ' ':
-            time.sleep(1)
+    playsound = input('do u want to play morse code as sound? (y/n)')
+    if playsound.upper() == 'Y':
+        ts.sound(result)
 
 
 # Main call
